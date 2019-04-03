510d940f319c6bb114cf6ba7a0acbbe9be50db4d
Lachlan Shaw <c3298685@uon.edu.au>
2019-04-03 08:28:00 +0000

    master: working ceasar cipher with better commenting

--- CeasarCipher.c
+++ CeasarCipher.c
@@ -18,24 +18,28 @@ int key, i=0;
     scanf("%d", &key);
 
     
-    /* Changes message from lower case to uppercase*/
+    /* While loop Changes message from lower case to uppercase by testing if ascii value of the 
+    letters are between a and z and then taking away 32 to make them a capital letter*/
     
     while(message[i] != '\0') {
         if (message[i]>= 'a' && message[i] <= 'z')
         message[i] = message [i] - 32;
         ++i;
     }
-    //calls function to encrypt message.
+    //calls function to encrypt message and then print the result.
     encryption(message, key);
     printf("%s\n", message);
 }    
-//Function definition
+/*Function definition. This function has a while loop that adds the key to the i'th value to encrypt
+the message. It also has if statements that test whether the input message has symbols in it and
+does not encrypt them.
+*/
 void encryption (char *x,int key){
     int i = 0; 
     while (x[i] != '\0')    {
         x[i] = x[i] + key;
         if(x[i] > 'Z')  {
-            if(x[i] > 91 + key)  {//symbols above Z will not change
+            if(x[i] > 91 + key)  {//symbols above Z will not be encrypted
                 x[i] = x[i] - key + 26;
             }
             x[i] = x[i] - 26;
@@ -43,7 +47,7 @@ void encryption (char *x,int key){
 
 
         else if(x[i] < 'A')   {
-            if(x[i]  < 65 + key) { //symbols below A will not change
+            if(x[i]  < 65 + key) { //symbols below A will not be encrypted
                 x[i] = x[i] - key - 26;
             }
             x[i] = x[i] +26;
