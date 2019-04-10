8ed25f6df19f8f4fa73163b517d152a83684b0ff
Lachlan Shaw <c3298685@uon.edu.au>
2019-04-10 04:51:39 +0000

    Neater indenting

--- CeasarCipher.c
+++ CeasarCipher.c
@@ -12,6 +12,7 @@ char message[100];
 int key, i=0;
 
     /*Prompts user to enter message and key and stores them as string array and integer value k*/
+   
     printf("Enter message you want encrypted\n");
     scanf("%s", message);
     printf("Enter the encryption key between 0 and 26\n");
@@ -35,16 +36,16 @@ int key, i=0;
     via the first while if statements. If a symbol is found it sends out the value and continues to the next i value.
     The capital letters left over are then given a value from A=0 to Z=26 by subtracting 65 from the ascii value.
     The encryption key is then added and modulus 26 used which makes letters greater than 25 rotate back to A */
-void encryption (char *x,int key){
+void encryption (char *x,int key) {
     int i = 0; //initialise integer i to 0 to allow it to start at first array input
     while (x[i] != '\0')    {//while the i'th value does not equal null the loop continues
         if (x[i] < 'A' || x[i] > 'Z'){//All symbols below the ascii 'A' and above ascii 'Z' value are not encrypted 
             x[i] = x[i];
             i++;// adds 1 onto i to move onto the next value
         }
-     else {//while the i'th value of message does not equal null the loop continues
-        x[i] = ((x[i] - 65 + key ) % 26) + 65;//The letters are given a value between 0 and 25 then encrypted and given back there original ascii number.
-        i++;
-        }
+        else {//while the i'th value of message does not equal null the loop continues
+            x[i] = ((x[i] - 65 + key ) % 26) + 65;//The letters are given a value between 0 and 25 then encrypted and given back there original ascii number.
+            i++;
+        }   
     }
 }
