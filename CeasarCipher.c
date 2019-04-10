90a0b836a6fdb139f184910e599891531d7e8d23
Lachlan Shaw <c3298685@uon.edu.au>
2019-04-10 04:23:54 +0000

    Ceasar cipher commenting complete

--- CeasarCipher.c
+++ CeasarCipher.c
@@ -32,8 +32,9 @@ int key, i=0;
     printf("%s\n", message);
 }    
 /*  Function definition. This function has a while loop that checks if the i'th value is a symbol or a letter
-    via the first if else if statements. If a symbol is found it sends out the value and continues to the next i value.
-    for letters it then adds the encryption key to the i'th value to encrypt and then sends its value out. */
+    via the first while if statements. If a symbol is found it sends out the value and continues to the next i value.
+    The capital letters left over are then given a value from A=0 to Z=26 by subtracting 65 from the ascii value.
+    The encryption key is then added and modulus 26 used which makes letters greater than 25 rotate back to A */
 void encryption (char *x,int key){
     int i = 0; //initialise integer i to 0 to allow it to start at first array input
     while (x[i] != '\0')    {//while the i'th value does not equal null the loop continues
@@ -42,7 +43,7 @@ void encryption (char *x,int key){
             i++;// adds 1 onto i to move onto the next value
         }
      else {//while the i'th value of message does not equal null the loop continues
-        x[i] = ((x[i] - 65 + key ) % 26) + 65;
+        x[i] = ((x[i] - 65 + key ) % 26) + 65;//The letters are given a value between 0 and 25 then encrypted and given back there original ascii number.
         i++;
         }
     }
