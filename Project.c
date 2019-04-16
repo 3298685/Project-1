d6e9660ead424d39489f463467e82c19c98156cf
Lachlan Shaw <c3298685@uon.edu.au>
2019-04-16 04:06:07 +0000

    encrypt and decrypt both working

--- Project.c
+++ Project.c
@@ -8,21 +8,42 @@ void decryption(char *x, int key);
 
 int main() {
 
-    
+     char message[1000];
+     int key, i=0;
+
+    FILE * input;
+    FILE * output;
+    input = fopen("input.txt", "r");
+    output = fopen ("output.txt", "w");
+    fscanf(input, "%d\n%[^\n]s", &key, message);
+    if(message == NULL) {
+        perror("fopen()");
+    return 0;
+    }
+      
+    while(message[i] != '\0') {//while the i'th value of message does not equal null the loop continues
+        if (message[i]>= 'a' && message[i] <= 'z')
+        message[i] = message [i] - 32;
+        ++i;
+    }
     
     switch (Task) {
         case '1': {
-            void CaesarEncrypt ();//calls encryption function
+            encryption(message, key);
+            printf("%s\n", message);
+            fprintf(output,"%s", message);
             break; }
         case '2': {
-            void CaesarDecrypt ();//calls decryption function
+            decryption(message, key);
+            printf("%s\n", message);//calls decryption function
+            fprintf(output,"%s", message);
             break; }
         
     }
 
 }
 // Caesar encryption function------------------------------------------------------------------
-void CaesarEncrypt()  {
+/*void CaesarEncrypt()  {
     
     // Initialise variables and string
     char message[1000];
@@ -40,8 +61,8 @@ void CaesarEncrypt()  {
 
 
     
-    /* While loop Changes message from lower case to uppercase by testing if ascii value of the 
-    letters are between a and z and then taking away 32 to make them a capital letter*/
+    /-* While loop Changes message from lower case to uppercase by testing if ascii value of the 
+    letters are between a and z and then taking away 32 to make them a capital letter Add /-* here
     
     while(message[i] != '\0') {//while the i'th value of message does not equal null the loop continues
         if (message[i]>= 'a' && message[i] <= 'z')
@@ -53,7 +74,7 @@ void CaesarEncrypt()  {
     encryption(message, key);
     printf("%s\n", message);
  
-} 
+} */
 /*  Function definition. This function has a while loop that checks if the i'th value is a symbol or a letter
     via the first while if statements. If a symbol is found it sends out the value and continues to the next i value.
     The capital letters left over are then given a value from A=0 to Z=26 by subtracting 65 from the ascii value.
@@ -73,8 +94,8 @@ void encryption (char *x,int key){
 }
 //End of caesar encryption function----------------------------------------------------------------
 
-// Start of caesar decryption function-------------------------------------------------------------------
-void CaesarDecrypt()  {
+// Start of caesar decryption function------------------------------------------------------------------- 
+/*void CaesarDecrypt()  {
     
     // Initialise variables and string
     char message[100];
@@ -89,8 +110,8 @@ return;
 
 fscanf(input, "%d\n%[^\n]s", &key, message);
     
-    /* While loop Changes message from lower case to uppercase by testing if ascii value of the 
-    letters are between a and z and then taking away 32 to make them a capital letter*/
+    /-* While loop Changes message from lower case to uppercase by testing if ascii value of the 
+    letters are between a and z and then taking away 32 to make them a capital letter*-/
     
     while(message[i] != '\0') {//while the i'th value of message does not equal null the loop continues
         if (message[i]>= 'a' && message[i] <= 'z')
@@ -101,7 +122,7 @@ fscanf(input, "%d\n%[^\n]s", &key, message);
     //calls function to encrypt message and then print the result.
     decryption(message, key);
     printf("%s\n", message);
-} 
+} */
 /*  Function definition. This function has a while loop that checks if the i'th value is a symbol or a letter
     via the first while if statements. If a symbol is found it sends out the value and continues to the next i value.
     The capital letters left over are then given a value from A=0 to Z=26 by subtracting 65 from the ascii value.
@@ -113,6 +134,18 @@ void decryption (char *x,int key) {
             x[i] = x[i];
             i++;// adds 1 onto i to move onto the next value
         }
+          else {//while the i'th value of message does not equal null the loop continues
+            int a;
+            a = (x[i] - 65 - key );
+            if (a < 0) {
+             a = a + 26;
+            x[i] = a % 26 + 65;
+            }
+            else 
+            x[i] = a % 26 + 65;//The letters are given a value between 0 and 25 then encrypted and given back there original ascii number.
+            i++;
+        }   
+
     }
 }
 // End of caesar decryption-------------------------------------------------------------------
